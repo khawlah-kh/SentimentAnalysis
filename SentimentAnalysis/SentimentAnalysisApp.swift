4fffbba72dcb1f1a277296adf7da3beace184081
Khawlah Khalid <khawlah1920@hotmail.com>
2023-01-04 08:09:27 +0300

    Initial Commit

---
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisApp.swift
+//  SentimentAnalysis
+//
+//  Created by Khawlah Khalid on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
